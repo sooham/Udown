98c3f8e47c865174b4b415e4cf45246451ca2596
sooham <rafizsooham@gmail.com>
2015-07-18 20:15:25 -0400

    Added ability to add non-user students to google map

--- googleMapsInviteSys/importData.js
+++ googleMapsInviteSys/importData.js
@@ -4,12 +4,12 @@
  * {username: string, realname: string, location: {lat: number, lng: number}}
  */
 
- function showWorld(map) {
+function showWorld(map) {
   /* Changes map to show world view
    */
-   map.setCenter(new google.maps.LatLng(0, 0));
-   map.setZoom(2);
- }
+  map.setCenter(new google.maps.LatLng(0, 0));
+  map.setZoom(2);
+}
 
 function geolocateUser(map) {
   /* Centres map to user's geolocation and sets a marker, if the user
@@ -40,10 +40,38 @@ function geolocateUser(map) {
   }
 }
 
-function initialize() {
+function addMarker(map, user) {
+  /* adds a marker for every user with  info window
+   * to map.
+   */
+  setTimeout(function() {
+    // add marker for user
+    var marker = new google.maps.Marker({
+      title: user.realname,
+      map: map,
+      position: user.position,
+      animation: google.maps.Animation.DROP
+    });
+    // add info window
+    var infowindow = new google.maps.InfoWindow({
+      content: 'hello'
+    });
+
+    // add click listener
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
+    });
+  }, 20);
+}
+
+function initialize(userArray) {
   /* Initializes google Map object in DOM with ID 'map-canvas'
-   * centred at the users location. For the map to correctly show, the
-   * user must accept the prompt asking for location services.
+   * centred at the users location. For the map to show locally, the
+   * user must accept the prompt asking for location services. Otherwise
+   * we show a global view.
+   *
+   * The function iterates over the array of user objects and adds a marker
+   * for each on the map
    */
   var mapOptions = {zoom: 13};
   // make the map
@@ -52,73 +80,23 @@ function initialize() {
 
   // use HTML5 geolocation to set the center of the map as user's location
   geolocateUser(map);
+
+  // now iterate over userArray and add each to map as marker
+  userArray.forEach(function(user) {addMarker(map, user);});
+
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+var name = ['bill gates', 'steve jobs', 'mark zuckerberg', 'drew houston',
+            'satya nadella', 'bob jones'];
 
+var users = [];
+for (var i = 0; i < 100; i++) {
+  var usrObj = {
+    realname: name[Math.random() * name.length],
+    position: {lat: (2 * Math.random() - 1) * 90, lng: (2 * Math.random() - 1) * 180},
+    username: 'haxx0r'
+  }
+  users.push(usrObj);
+}
 
-
-
-
-
-//   //###################
-//   var myLatlng = {lat: -30.397, lng: 150.644};
-//   var mapOptions = {
-//     zoom: 8,
-//     center: myLatlng
-//   };
-
-//   // make the map
-//   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-//   // make the info window for marker on click
-//   var contentString = '<div id="content">'+
-//       '<div id="siteNotice">'+
-//       '</div>'+
-//       '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-//       '<div id="bodyContent">'+
-//       '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-//       'sandstone rock formation in the southern part of the '+
-//       'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-//       'south west of the nearest large town, Alice Springs; 450&#160;km '+
-//       '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-//       'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-//       'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-//       'Aboriginal people of the area. It has many springs, waterholes, '+
-//       'rock caves and ancient paintings. Uluru is listed as a World '+
-//       'Heritage Site.</p>'+
-//       '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-//       'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-//       '(last visited June 22, 2009).</p>'+
-//       '</div>'+
-//       '</div>';
-
-//   var infowindow = new google.maps.InfoWindow({
-//       content: contentString
-//   });
-
-//   // finally add the marker
-//   var marker = new google.maps.Marker({
-//       position: myLatlng,
-//       map: map,
-//       title: 'hello'
-//   });
-//   // add click listener
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.open(map,marker);
-//   });
-
-// google.maps.event.addDomListener(window, 'load', initialize);
-
-
-// google.maps.Infowindow({content: "abd", maxWidth: 100});
-
-// use to drop an array of markers in sequence
-
-// function drop() {
-//   for (var i =0; i < markerArray.length; i++) {
-//     setTimeout(function() {
-//       addMarkerMethod();
-//     }, i * 200);
-//   }
-// }
+google.maps.event.addDomListener(window, 'load', function() {initialize(users);});
