babe0a9a31dbd7e12b0178f97b4fb6b1582981b7
sooham <rafizsooham@gmail.com>
2015-07-18 22:41:14 -0400

    Prettification of serverside code braintree

--- UdownService/braintree.py
+++ UdownService/braintree.py
@@ -1,14 +1,16 @@
 import braintree
 import django
-braintree.Configuration.configure(braintree.Environment.Sandbox,
-                                  merchant_id="5rkq7p4h66b848hz",
-                                  public_key="txp62v4fqh966gcb",
-                                  private_key="bf94f5f34c699735666b79af0ea85dd0")
+braintree.Configuration.configure(
+  braintree.Environment.Sandbox, merchant_id="5rkq7p4h66b848hz",
+  public_key="txp62v4fqh966gcb", private_key="bf94f5f34c699735666b79af0ea85dd0"
+)
 
-@app.route("/client_token", methods=["GET"]) 
+
+@app.route("/client_token", methods=["GET"])
 
 def client_token():
     return braintree.ClientToken.generate()
+
 
 @app.route("/payment-methods", methods=["POST"])
 def create_purchase():
